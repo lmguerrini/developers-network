baa6c8b0e1ec29f396a08576200ba32869d60e27
Lorenzo <lorenzomariaguerrini@gmail.com>
2021-10-14 20:31:09 +0200

    module renamed

--- client/src/otherProfile.js
+++ client/src/otherProfile.js
@@ -41,7 +41,6 @@ export default class OtherProfile extends Component {
         console.log("----sendDataToParent", this.state.profile_pic);
         props.parentCallback(this.state.profile_pic);
     } */
-    
     /* sendDataToParent(e) {
         console.log("----sendDataToParent", this.state.profile_pic);
         this.props.parentCallback(this.state.profile_pic);
